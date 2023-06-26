d748e44f6ef9c2ac2385d08c904b1957819299c2
onFire(Abhi) <40654066+AbhiShake1@users.noreply.github.com>
2023-06-26 23:16:13 +0545

    implement api calls

--- app/src/main/java/com/app/catfacts/MainActivity.kt
+++ app/src/main/java/com/app/catfacts/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.app.catfacts
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Colors
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import com.app.catfacts.services.apiService
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.fetch)
+        btn.setOnClickListener {
+            onFetch()
+        }
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun onFetch(): Unit {
+        GlobalScope.launch(Dispatchers.Main) {
+            val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+            val contentView = findViewById<TextView>(R.id.content)
+            progressBar.isVisible = true
+
+            val response = apiService.getFacts()
+            if(response.isSuccessful){
+                contentView.setTextColor(Color.GREEN)
+                response.body()?.apply {
+                    val text = mapIndexed{i, v -> "$i: ${v.text}"}.joinToString { "$it\n" }
+                    contentView.text = text
+                }
+            }else{
+                contentView.setTextColor(Color.RED)
+                contentView.text = response.errorBody()?.toString()
+            }
+
+            progressBar.isVisible = false
+        }
     }
 }
--- app/src/main/java/com/app/catfacts/services/restClient.kt
+++ app/src/main/java/com/app/catfacts/services/restClient.kt
@@ -2,9 +2,18 @@ package com.app.catfacts.services
 
 import com.app.catfacts.models.dataModels.CatsModel
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-interface RestClient {
+public interface RestClient {
     @GET("facts/")
     suspend fun getFacts(): Response<List<CatsModel>>
 }
+
+private val retrofit: Retrofit = Retrofit.Builder()
+    .baseUrl("https://cat-fact.herokuapp.com/")
+    .addConverterFactory(GsonConverterFactory.create())
+    .build()
+
+public val apiService = retrofit.create(RestClient::class.java)
